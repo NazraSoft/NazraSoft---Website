2fcef64a3a17b2c0b9c614bc15988ba45475b498
Ritik Ranjan <ritik123453@gmail.com>
2022-10-18 02:05:20 +0530

    Navbar created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from 'next/app'
 
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return  <ThemeProvider enableSystem={true} attribute="class">
-  <Component {...pageProps} />
-</ThemeProvider>
+  return <ThemeProvider enableSystem={true} attribute="class">
+
+
+    <Component {...pageProps} />
+
+  </ThemeProvider>
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -27,12 +27,10 @@ const Home: NextPage = () => {
       <header className="text-gray-600 body-font bg-green-400 mt-10 ml-20 mr-20 rounded-tl-xl rounded-br-xl shadow-md">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
+            {/* logo */}
             <span className="ml-3 text-xl">Art Life</span>
           </a>
-          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4	flex flex-wrap items-center text-base justify-center">
+          <nav className="hidden  md:mr-auto md:ml-4 md:py-1 md:pl-4	md:flex flex-wrap items-center text-base justify-center">
             <a className="mr-5 hover:text-white text-black ">Home</a>
             <a className="mr-5 hover:text-white text-black">About us</a>
             <a className="mr-5 hover:text-white text-black ">Blog</a>
@@ -40,7 +38,7 @@ const Home: NextPage = () => {
             <a className="mr-5 hover:text-white text-black ">Contact</a>
             {renderThemeChanger()}
           </nav>
-          <div className='border-1 border-black rounded-lg shadow-md w-80 h-9 bg-black'>
+          <div  className='hidden lg:block border-1 border-black rounded-lg shadow-md w-80 h-9 bg-black'>
             <div className= 'flex flex-row  focus:border-blue-700 w-[97%] h-[80%] border-1 mx-1 mt-1'>
                <input className='flex w-[93%] h-[96%] border-1 bg-black mx-1 p-2' type="search" name="search" placeholder='Search Here' />
                <FaSearch className='mt-1 w-5 h-5' />
