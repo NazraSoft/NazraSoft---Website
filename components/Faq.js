a1dd4efd011a4ffccc0cd80d5050afc1ff7e9750
Ritik Ranjan <ritik123453@gmail.com>
2022-11-26 14:20:02 +0530

    Faq page

--- components/Faq.js
+++ components/Faq.js
@@ -3,40 +3,90 @@ import React from 'react'
 const Faq = () => {
   return (
     <div>
-        <div className="py-20 dark:bg-gray-600">
-  <h1 className="text-4xl font-extrabold text-center text-gray-700 dark:text-gray-50">Frequently Asked Questions</h1>
+      <div className="py-20 dark:bg-gray-600">
+        <h1 className="text-4xl font-extrabold text-center text-gray-700 dark:text-gray-50">Frequently Asked Questions</h1>
 
-  <div className="grid grid-cols-1 gap-10 mx-4 mt-20 md:grid-cols-2 md:mx-40">
-    <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
-      <h2 className="font-semibold text-md">What does Lifetime Access exactly mean?</h2>
-      <p className="mt-4 text-sm font-light leading-6">Tailwind Master Kits' lifetime access gives you access to the components and the code with no additional recurring charges. Pay once, use the components forever. Also, if in the future more components are added, you get access to them if you're subscribed for the lifetime access membership.</p>
-    </div>
-    <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
-      <h2 className="font-semibold text-md">How is One-Year access pass different from Lifetime Access?</h2>
-      <p className="mt-4 text-sm font-light leading-6">With One-Year pass, you get access to all the premium components for One Year and all the updates are for One Year only. With Lifetime Access, you get access to the entire set for features for Lifetime - Which means you can get future updates for the components as long as the website is live.</p>
-    </div>
-    <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
-      <h2 className="font-semibold text-md">Can I resell the components on my own website?</h2>
-      <p className="mt-4 text-sm font-light leading-6">No. You cannot resell the components on your own website claiming that you built them. You cannot create templates out of Tailwind Master Kit's components and sell them. You can use the components for client projects and personal use.</p>
-    </div>
-    <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
-      <h2 className="font-semibold text-md">What are Free Updates?</h2>
-      <p className="mt-4 text-sm font-light leading-6">We are tirelessly working on adding more quality components on the platform. Most of our components are premium but we do release free components and keep on updating them regularly. Lookout for our newsletter, we release new components every month.</p>
-    </div>
+        <div className="grid grid-cols-1 gap-10 mx-4 mt-20 md:grid-cols-2 md:mx-40">
+          <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
+            <h2 className="font-semibold text-md">What advantages of outsourcing your project to our team you get?</h2>
+            <p className="mt-4 text-sm font-light leading-6"><li>More time to stay focused on the development of your business  </li>
+              <li>High Professional Development Team</li>
+              <li>The product-oriented approach</li>
+              <li>Cost Savvy Solution </li>
+              <li>Control Over Your IT Costs</li>
+            </p>
+          </div>
+          <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
+            <h2 className="font-semibold text-md">What are the main industries we serve?</h2>
+            <p className="mt-4 text-sm font-light leading-6">To provide the best services in the market we mainly focus our software development outsourcing services on product development for several primary industries:
+              <li>E-commerce Development Solutions</li>
+              <li>Healthcare Application Development</li>
+              <li>Software Development For Real Estate </li>
+              Besides, we have a lot of successful projects in other industries like entertainment, financial, hospitality, media, education and other.
+            </p>
+          </div>
+          <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
+            <h2 className="font-semibold text-md">What are the software outsourcing services we provide?</h2>
+            <p className="mt-4 text-sm font-light leading-6">
+              We propose the next set of  software and design development services separately or within the full project development life cycle:
+              <li>Branding and UI/UX Design</li>
+              <li>Web Development</li>
+              <li>Mobile App Development</li>
+              <li>QA Services</li>
+              <li>Blockchain</li>
+              <li>DevOps</li>
+              <li>Project Management</li>
+              <li>Product Management</li>
+            </p>
+          </div>
+          <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
+            <h2 className="font-semibold text-md">What technologies do we use in software development?</h2>
+            <p className="mt-4 text-sm font-light leading-6">Our team includes only high-experienced professionals who have experience in different software development fields:
+              For web and mobile application development:
+              <div className='md:flex flex-row'>
 
-    <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
-      <h2 className="font-semibold text-md">On how many projects can I use Tailwind Master Kit's components?</h2>
-      <p className="mt-4 text-sm font-light leading-6">With Tailwind Master Kit's Lifetime Access Pass, you can use the components on as many websites as you want - which is unlimited. As long as its just you who is using the licence, use it on unlimited projects.</p>
-    </div>
+              <li className='mx-2'>HTML5, CSS3</li>
+              <li className='mx-2'>JavaScript</li>
+              </div>
+              <div className='md:flex flex-row'>
 
-    <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
-      <h2 className="font-semibold text-md">What is community access and how is it beneficial for me?</h2>
-      <p className="mt-4 text-sm font-light leading-6">You get access to our private Discord Server where you can request for custom components, seek help from the makers of Tailwind Master Kit and other connect and network with other like minded people who are already using the product.</p>
-    </div>
-  </div>
-</div>
+              <li className='mx-2'>React JS</li>
+              <li className='mx-2'>Next Js</li>
+              </div>
+              <div className='md:flex flex-row'>
+
+              <li className='mx-2'>Node Js</li>
+              <li className='mx-2'>Bootstrap</li>
+              </div>
+              <div className='md:flex flex-row'>
+
+              <li className='mx-2'>Web API Services</li>
+              <li className='mx-2'>Java</li>
+              </div>
+              <div className='md:flex flex-row'>
+
+              <li className='mx-2'>Kotlin</li>
+              <li className='mx-2'>React Native</li>
+              </div>
+              Thus, if you’d like to implement some recent technologies into your project and make it look updated, outsourcing to our specialists experienced in that niche will be the best option.
+            </p>
+          </div>
+
+          <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
+            <h2 className="font-semibold text-md">How long does it take to start my product development?</h2>
+            <p className="mt-4 text-sm font-light leading-6">To start with, we review project requirements from the client, we get them analyzed and then inform you of the estimation results.
+              Once you are fine with the cost and time, the next step would be to create a project report document and a proposal. Our business analyst will prepare this and then we send you the same for review. Once everything has been finalized we would then take the project to the next stage, which is the development stage.
+            </p>
+          </div>
+
+          <div className="p-4 text-gray-800 bg-gray-100 rounded-md dark:text-gray-50 dark:bg-gray-700">
+            <h2 className="font-semibold text-md">How do we assure privacy and confidentiality?</h2>
+            <p className="mt-4 text-sm font-light leading-6">We sign NDAs and Confidentiality Agreements as required by you. All our employees are full-time employees and are bound by company Confidentiality and Non-Disclosure clauses.
+              Additionally, for sensitive projects, we can sign custom NDAs.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
-
 export default Faq
