bf811c29895cea8e93fe5105099f7caf7acdc416
Ritik Ranjan <ritik123453@gmail.com>
2022-12-28 23:19:13 +0530

    Pincode added

--- src/_codux/boards/about/about.board.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import about from '../../../../pages/about';
-
-export default createBoard({
-    name: 'about',
-    Board: () => <about />
-});
