e41cfaf70fa0fbdba93df4b5aa482c46b8c5a947
Ritik Ranjan <75849689+ritikranjan12@users.noreply.github.com>
2022-12-31 04:59:57 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://zajjiytoxifltgikdxdy.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphamppeXRveGlmbHRnaWtkeGR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkwMzY0ODMsImV4cCI6MTk4NDYxMjQ4M30.p0Hte_CCqR8lcOIOlLaiY-IlEWjjQgim2q44F4QxZXU')
+const supabase = createClient('', '')
 
 export default supabase;
